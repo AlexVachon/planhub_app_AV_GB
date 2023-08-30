9ab46acb7d75399908d18f3edaa0d7d756f78d00
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2023-08-30 16:00:16 -0400

    +scss -> css

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Gestionnaire de route pour la page d'accueil
 app.get('/', (req, res) => {
-  res.send('Page en construction !');
+  res.sendFile(path.join(__dirname, 'public/templates/index.html'));
 });
 
 // Écoute du serveur sur le port spécifié
 app.listen(port, () => {
   console.log(`Le serveur écoute sur le port ${port}`);
-  console.log(`---> http://localhost:${port}/`);
-  console.log(`CTRL+C to stop code`);
+  console.log(`---> http://localhost:${port}/ <---`);
+  console.log(`CTRL + C to STOP code`);
 });
