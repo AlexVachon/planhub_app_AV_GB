39d219a4b06188c37b3799a8d7cb72256eeefdf1
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2023-08-30 10:26:04 -0400

    *app.listen -> ajout link serveur

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const port = 3000;
 
 // Gestionnaire de route pour la page d'accueil
 app.get('/', (req, res) => {
-  res.send('Bienvenue sur votre application Express !');
+  res.send('Page en construction !');
 });
 
 // Écoute du serveur sur le port spécifié
 app.listen(port, () => {
   console.log(`Le serveur écoute sur le port ${port}`);
-  console.log(`http://localhost:${port}/`);
+  console.log(`---> http://localhost:${port}/`);
+  console.log(`CTRL+C to stop code`);
 });
