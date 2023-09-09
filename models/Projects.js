a476af28139e4b239f147d697ad2dda61cddad21
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2023-09-09 16:41:15 -0400

    Creation model pour les projets + petits changements dans les Users

---
+++ models/Projects.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const ProjectsModel = mongoose.Schema({
+
+    "project_name": {
+        type: String,
+        required: [true, "Vous devez fournir un nom de projet"],
+        trim: true,
+        maxlenght: [50, "Votre nom de projet ne peut pas dépasser 50 caractères"]
+    },
+    "created_by":{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users',
+        required: true
+    },
+    "admins": [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users'
+    }],
+    "users": [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users'
+    }],
+    "tasks": [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Tasks'
+    }],
+    "created_at": {
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+module.exports = mongoose.Model('Projects', ProjectsModel)
--- models/Users.js
+++ models/Users.js
@@ -6,25 +6,25 @@ const UserModel = new mongoose.Schema({
         type: String,
         required: [true, "Vous devez fournir un prénom"],
         trim: true,
-        maxlength: [50, "Votre prénom ne peut dépasser 50 caractères"]
+        maxlength: [50, "Votre prénom ne peut pas dépasser 50 caractères"]
     },
     "last_name":{
         type: String,
         required: [true, "Vous devez fournir un nom"],
         trim: true,
-        maxlength: [50, "Votre nom ne peut dépasser 50 caractères"]
+        maxlength: [50, "Votre nom ne peut pas dépasser 50 caractères"]
     },
     "username": {
         type: String,
         required: [true, "Vous devez fournir un Username"],
         trim: true,
-        maxlength: [15, "Votre Username ne peut dépasser 15 caractères"]
+        maxlength: [15, "Votre Username ne peut pas dépasser 15 caractères"]
     },
     "password":{
         type: String,
         required: [true, "Vous devez fournir un mot de passe"],
         trim: true,
-        maxlength: [15, "Votre mot de passe ne peut dépasser 15 caractères"]
+        maxlength: [15, "Votre mot de passe ne peut pas dépasser 15 caractères"]
     },
     "projects":{
         type: Array
