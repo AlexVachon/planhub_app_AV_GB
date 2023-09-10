c9865e31717e137222684dd2562546f8c7402cd1
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2023-09-10 16:50:09 -0400

    * champs "task_type"

--- models/Tasks.js
+++ models/Tasks.js
@@ -1,18 +1,29 @@
 const mongoose = require('mongoose')
 
+// Change id needed
+const taskTypeOptions = ['À faire','En cours','En attente','À vérifier','En pause','Complété', 'Annulé']
+
+//Check if value is in taskTypeOptions
+const validateTaskType = (value) => {
+    return taskTypeOptions.includes(value)
+}
+
 const TasksModel = mongoose.Schema({
     "task_name":{
         type: String,
         maxlength: [25, 'Le nom de la tâche ne peut pas dépasser 25 caractères'],
-        required: true
-    },
-    "task_description":{
-        type: String
+        required: [true, 'Le nom de la tâche est requis']
     },
     "task_type":{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'TypesTask',
-        required: true
+        type: String,
+        required: [true, "Le champ type est requis"],
+        validate: {
+          validator: validateTaskType,
+          message: "La valeur 'type' de la tâche n'est pas valide",
+        }
+    },
+    "task_description":{
+        type: String,
     },
     "created_by":{
         type:mongoose.Schema.Types.ObjectId,
