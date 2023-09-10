e81c2cb9723e751ba55f348a4646be606631e89b
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2023-09-10 17:13:07 -0400

    *const User -> const Users

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,11 @@
-const User = require('../models/Users')
+const Users = require('../models/Users')
 
 
 const getOneUser = async (req, res) => {
     //Valeur des champs dans le formulaire **FAIRE EN POST**
     const {user_email, user_password} = req.body
     try{
-        const user = await User.findOne({'email': user_email, 'password': user_password})
+        const user = await Users.findOne({'email': user_email, 'password': user_password})
         if (user){
             res.status(201).json({user})
         }else{
