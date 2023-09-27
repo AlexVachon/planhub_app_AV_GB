2055b29b7b92d0e9cf18db6bd619b07b1431e259
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2023-09-27 17:40:11 -0400

    +Feedbacks

--- routes/join.js
+++ routes/join.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const {loadPage, setSession, createUser, loadCreate} = require('../controllers/join')
+const {loadPage, setSession, createUser, loadCreate, confirmEmail} = require('../controllers/join')
 
 
 router.route('/').get(loadPage)
+router.route('/confirm/email').post(confirmEmail)
 router.route('/login').post(setSession)
 router.route('/sign').get(loadCreate).post(createUser)
 
