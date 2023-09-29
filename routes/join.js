1a966defa6ce510d33b2a2948e127f698b8409b3
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2023-09-29 11:03:58 -0400

    +/confirm/email_used, /confirm/username_used

--- routes/join.js
+++ routes/join.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const {loadPage, setSession, createUser, loadCreate, confirmEmail} = require('../controllers/join')
+const { loadPage, setSession, loadCreate, confirmEmail, isEmailUsed, isUserNameUsed } = require('../controllers/join')
+const { createUser } = require('../controllers/users')
 
 
 router.route('/').get(loadPage)
 router.route('/confirm/email').post(confirmEmail)
+router.route('/confirm/email_used').post(isEmailUsed)
+router.route('/confirm/username_used').post(isUserNameUsed)
 router.route('/login').post(setSession)
 router.route('/sign').get(loadCreate).post(createUser)
 
