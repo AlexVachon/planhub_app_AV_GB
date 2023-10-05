10ba1a5156f34f738ca7b23e0f5ca030173d7bf6
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2023-10-05 16:22:46 -0400

    *getOneUser, *createUser

--- controllers/users.js
+++ controllers/users.js
@@ -2,22 +2,28 @@ const Users = require('../models/Users')
 const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
+const regexFNLN = /^.{3,50}$/
+const regexUN = /^[a-zA-Z0-9_]{8,20}$/
+const regexEM = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const regexPW = /^.{8,}/
 
 const getOneUser = async (req, res) => {
-    //Valeur des champs dans le formulaire **FAIRE EN POST**
     const { user_email, user_password } = req.body
     try {
         const user = await Users.findOne({ 'email': user_email })
         if (user == null) {
-            res.status(400).json({message: 'Courriel incorrect' })
+            res.status(400).json({ message: 'Courriel incorrect' })
         }
 
         const passwordMatch = await bcrypt.compare(user_password, user.password);
 
         if (passwordMatch) {
-            res.status(201).json({ message: `Connecté en tant que ${user.username}`, user })
+            req.session.authenticated = true
+            console.log(user._id)
+            req.session.user = user._id
+            res.status(201).redirect('/')
         } else {
-            res.status(400).json({ message: "Mot de passe incorrect"})
+            res.status(400).json({ message: "Mot de passe incorrect" })
         }
 
     } catch (err) {
@@ -39,7 +45,8 @@ const getAllUsers = async (req, res) => {
 
 const createUser = async (req, res) => {
     const { first_name, last_name, username, email, password, password_confirm } = req.body
-    if (password == password_confirm) {
+    if (regexFNLN.test(first_name) && regexFNLN.test(last_name) &&
+        regexUN.test(username) && regexEM.test(email) && regexPW.test(password) && password == password_confirm) {
         try {
             const user = new Users({
                 _id: new mongoose.Types.ObjectId(),
@@ -48,16 +55,30 @@ const createUser = async (req, res) => {
                 username: username.toString(),
                 email: email.toString(),
                 password: password.toString()
-                
+
             })
             console.log(user)
-            user.save()
-            res.status(201).redirect('/join')
+            user.save().then(() => {
+                res.status(201).redirect('/join')
+            }).catch((err) => {
+                if (err.name === 'ValidationError') {
+                    const validationErrors = {};
+                    for (const key in err.errors) {
+                        validationErrors[key] = err.errors[key].message;
+                    }
+                    console.error(validationErrors);
+                    res.status(400).json({ errors: validationErrors });
+                } else {
+                    console.error(err);
+                    res.status(500).json({ message: 'Une erreur interne est survenue.' });
+                }
+            })
+
         } catch (err) {
-            console.error("Erreur de la création de l'utilisateur :", err)
+            console.error(err)
         }
     } else {
-        res.status(400).json({ message: "Les mots de passe ne sont pas identiques." })
+        res.status(400).json({ message: "Au moins un des champs est incorrects..." })
     }
 }
 
